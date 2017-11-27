7c388d8844c27a5879a14acc2ff4d76d1ddcdcb0
Marton Revesz <martonrevesz@gmail.com>
2017-11-27 17:03:24 +0100

    feat(HelloUser): create

---
+++ week-02/day-01/HelloUser/HelloUser/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify this program to greet user instead of the World!
+            // The program should ask for the name of the user
+            Console.WriteLine("Please enter your name");
+            string input = Console.ReadLine();
+            Console.WriteLine("Your name is " + input);
+            Console.ReadLine();
+
+        }
+    }
+}
