43f312c7a78d8cf7425fb04f2eb75b6d6ffb69a0
Marton Revesz <martonrevesz@gmail.com>
2017-11-27 16:39:14 +0100

    feat(Cuboid): create

---
+++ week-02/day-01/Cuboid/Cuboid/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuboid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select a side length: ");
+            Double.TryParse(Console.ReadLine(), out double a);
+            Console.WriteLine("the surface of your cube is: " + 6 * a * a);
+            Console.WriteLine("the volume of your cube is: " + a * a * a);
+        }
+    }
+}
