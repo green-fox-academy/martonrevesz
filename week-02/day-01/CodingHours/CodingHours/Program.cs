5571ca0fb39476c11283f273f5adc22579f58180
Marton Revesz <martonrevesz@gmail.com>
2017-11-27 16:55:46 +0100

    feat(CodingHours): create

---
+++ week-02/day-01/CodingHours/CodingHours/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingHours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // An average Green Fox attendee codes 6 hours daily
+            // The semester is 17 weeks long
+            //
+            // Print how many hours is spent with coding in a semester by an attendee,
+            // if the attendee only codes on workdays.
+            //
+            // Print the percentage of the coding hours in the semester if the average
+            // work hours weekly is 52
+
+            double spentHours = 6 * 17 * 5;
+            Console.WriteLine("An avarage attendee codes " + spentHours + " during their education");
+            double percentage = 30 / (double) 52 * 100;
+            Console.WriteLine("An avarage attendee spends " + string.Format("{0:0.00}", percentage) + " percent of their work on coding");
+        }
+    }
+}
