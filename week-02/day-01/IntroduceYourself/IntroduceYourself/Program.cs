5accb195ea3a4fbcc5a9f4fa1d43fcaca28d4b2b
Marton Revesz <martonrevesz@gmail.com>
2017-11-27 15:30:21 +0100

    feat(IntroduceYourself): create

---
+++ week-02/day-01/IntroduceYourself/IntroduceYourself/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduceYourself
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Write a program that prints a few details to the terminal window about you
+			// It should print each detail to a new line.
+			//  - Your name
+			//  - Your age
+			//  - Your height in meters (as a decimal fraction)
+			//
+			//  Example output:
+			//  John Doe
+			//  31
+			//  1.87
+			string myName = "Marci Revesz";
+			int myAge = 25;
+			double myHeight = 1.73;
+			Console.WriteLine(myName);
+			Console.WriteLine(myAge);
+			Console.WriteLine(myHeight);
+		}
+	}
+}
