8c7d0472435a1af0bfcc08597fa57624fe36f3cc
Marton Revesz <martonrevesz@gmail.com>
2017-11-27 15:44:26 +0100

    feat(Swap): create

---
+++ week-02/day-01/Swap/Swap/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Swap the values of the variables
+			int a = 123;
+			int b = 526;
+			int temp = a;
+			a = b;
+			b = temp;
+			Console.WriteLine("a: " + a + "  b: " + b);
+			Console.ReadLine();
+		}
+	}
+}
