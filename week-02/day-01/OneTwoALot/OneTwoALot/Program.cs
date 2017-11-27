e778851c2dfd14f592a63b49bf668025d46b9cf2
Marton Revesz <martonrevesz@gmail.com>
2017-11-27 17:35:53 +0100

    feat(OneTwoALot): create

---
+++ week-02/day-01/OneTwoALot/OneTwoALot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneTwoALot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double myDouble = double.Parse(Console.ReadLine());
+            if (myDouble < 0)
+            {
+                Console.WriteLine("Not enough");
+            }
+            else if (myDouble == 1)
+            {
+                Console.WriteLine("One");
+            }
+            else if (myDouble == 2)
+            {
+                Console.WriteLine("Two");
+            }
+            else
+            {
+                Console.WriteLine("Lot");
+            }
+    }
+}
