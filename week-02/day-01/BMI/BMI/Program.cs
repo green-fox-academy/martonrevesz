66b7802e8af8019da4859beb62dfc3b9790b3d64
Marton Revesz <martonrevesz@gmail.com>
2017-11-27 15:52:53 +0100

    feat(BMI): create

---
+++ week-02/day-01/BMI/BMI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Print the Body mass index (BMI) based on these values
+		    double massInKg = 81.2;
+		    double heightInM = 1.78;
+		    Console.WriteLine("Your BMI is: " + massInKg / Math.Pow(heightInM, 2));
+		}
+	}
+}
