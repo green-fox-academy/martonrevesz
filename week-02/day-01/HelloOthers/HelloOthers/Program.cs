b1a0b634fbf5328adae82bd9b14c22673a103275
Marton Revesz <martonrevesz@gmail.com>
2017-11-27 15:19:29 +0100

    feat(HelloOthers, HumptyDumpty): create

---
+++ week-02/day-01/HelloOthers/HelloOthers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloOthers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello Andris");
+			Console.WriteLine("Hello Tamas");
+			Console.WriteLine("Hello Zsuzska");
+		}
+	}
+}
