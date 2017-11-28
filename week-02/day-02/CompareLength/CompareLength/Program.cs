6072d855d9fb015ba59f21a32da7cba5c11c7320
Marton Revesz <martonrevesz@gmail.com>
2017-11-28 12:29:47 +0100

    feat(CompareLength, Printer, Third): create

---
+++ week-02/day-02/CompareLength/CompareLength/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] p1 = { 1, 2, 3 };
+            int[] p2 = { 4, 5 };
+            Console.WriteLine("p1 has more element than p2: " + (p1.Length > p2.Length));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-02/day-02/Printer/Printer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            string[] myStringArray = { "hello", "green fox", "academy", "helloweekend" };
+			Printer(myStringArray);
+		}
+
+		static void Printer(string[] array)
+		{
+			foreach (string item in array)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
