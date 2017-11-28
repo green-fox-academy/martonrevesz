137e50a31e4e3bb88bcf156af03d17958d69c7c7
Marton Revesz <martonrevesz@gmail.com>
2017-11-28 11:04:28 +0100

    feat(Doubling): create

---
+++ week-02/day-02/Doubling/Doubling/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ak = 123;
+            Console.WriteLine(Doubling(ak));
+            Console.ReadLine();
+        }
+
+        static int Doubling(int number)
+        {
+            return number * 2;
+        }
+    }
+}
