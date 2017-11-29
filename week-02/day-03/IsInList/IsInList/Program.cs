b5b8d6197ec3187cf9bb6a42046148dad50c105d
Marton Revesz <martonrevesz@gmail.com>
2017-11-29 17:02:44 +0100

    feat(IsInList, QuoteSwap): create

---
+++ week-02/day-03/IsInList/IsInList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsInList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 2, 4, 6, 8, 10, 12, 14, 16 };
+            var checkList = new List<int> { 4, 8, 12, 16 };
+            bool containsAll = true;
+            foreach (int item in checkList)
+            {
+                // or check list.IndexOf return value 
+                if (!list.Contains(item))
+                {
+                    containsAll = false;
+                }
+            }
+            Console.WriteLine(" 'list' contains all the elements of 'checkList': " + containsAll);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-02/day-03/QuoteSwap/QuoteSwap/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuoteSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+            //original order:  0 1 2 3 4 5 6 7
+            //new order:       0 1 5 3 4 2 6 7
+            string temp = list[2];
+            list[2] = list[5];
+            list[5] = temp;
+            foreach (string item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
