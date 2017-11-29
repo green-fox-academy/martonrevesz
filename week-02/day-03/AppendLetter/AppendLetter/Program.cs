ee930c59e050375f6eee5c6f3ae9d779edfd62ce
Marton Revesz <martonrevesz@gmail.com>
2017-11-29 15:07:13 +0100

    feat(Appendletter, CandyShop): create

---
+++ week-02/day-03/AppendLetter/AppendLetter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppendLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            for (int i = 0; i < far.Count; i++)
+            {
+                far[i] += "a";
+                Console.WriteLine(far[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ week-02/day-03/CandyShop/CandyShop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CandyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            list.Remove(2);
+            list.Insert(1, "Croissant");
+            list.Remove(false);
+            list.Add("Ice cream");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
