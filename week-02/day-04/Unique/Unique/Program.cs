b62f7f8b16d3042dd5d907fd78b9eb49b64301f2
Marton Revesz <martonrevesz@gmail.com>
2017-11-30 16:51:58 +0100

    feat(Unique): create

---
+++ week-02/day-04/Unique/Unique/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>() {1, 11, 34, 11, 52, 61, 1, 34 };
+            Unique(list);
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        static List<int> Unique(List<int> list)
+        {
+            list.Sort();
+            for (int i = 0; i < (list.Count -1 ); i++)
+            {
+                if (list[i] == list[i+1])
+                {
+                    list.Remove(list[i]);
+                }
+            }
+            return list;
+        }
+    }
+}
