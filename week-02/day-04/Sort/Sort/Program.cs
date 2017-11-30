f44e83357cbacc7fd2d53d991b06b9ace9589d7c
Marton Revesz <martonrevesz@gmail.com>
2017-11-30 17:38:45 +0100

    feat(Sort): create

---
+++ week-02/day-04/Sort/Sort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>() {5 , 17, 34, 93, 17, 35, 31};
+            SortList(list);
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        static List<int> SortList(List<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)  
+            {
+                for (int j = i ; j < list.Count - 1 ; j++)
+                {
+                    if (list[i] > list[j + 1])
+                    {
+                        int temp = list[i];
+                        list[i] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+            return list; 
+        }
+    }
+}
