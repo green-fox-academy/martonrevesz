0d1edc4e1b696d374053b321f42ea30172078025
Marton Revesz <martonrevesz@gmail.com>
2017-11-30 12:34:56 +0100

    feat(Calculator): create

---
+++ week-02/day-03/Calculator/Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type in the expression with integers: 'number' 'operetion' ' 'number'");
+            string userInput = Console.ReadLine();
+            string[] strings = userInput.Split(' ');
+            int operand1 = Convert.ToInt32(strings[0]);
+            string operation = strings[1];
+            int operand2 = Convert.ToInt32(strings[2]); ;
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine(operand1 + operand2);
+                    break;
+                case "-":
+                    Console.WriteLine(operand1 - operand2);
+                    break;
+                case "*":
+                    Console.WriteLine(operand1 * operand2);
+                    break;
+                case "/":
+                    if (operand2 == 0)
+                    {
+                        Console.WriteLine("Invalid operation. The universe collapsed.");
+                        break;
+                    }
+                    Console.WriteLine(operand1 / operand2);
+                    break;
+                case "%":
+                    Console.WriteLine(operand1 % operand2);
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
