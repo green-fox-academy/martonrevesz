d081f7e5cfffec2df7411852f4f0b443d921463b
Marton Revesz <martonrevesz@gmail.com>
2017-11-30 16:07:26 +0100

    feat(DrawPyramid): create

---
+++ week-02/day-01/DrawPyramid/DrawPyramid/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DrawPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = i; j < number; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k <= 2 * i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
