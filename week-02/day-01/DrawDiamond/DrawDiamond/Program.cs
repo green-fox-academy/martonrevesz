5ccbec5a9e55ec8c6efc6e26715ae93437e8ef68
Marton Revesz <martonrevesz@gmail.com>
2017-11-30 21:52:05 +0100

    feat(DrawDiamond): create

---
+++ week-02/day-01/DrawDiamond/DrawDiamond/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DrawDiamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                //draw spaces 
+                double spaceDouble = Math.Abs(i - (number - 1) / 2.0);
+                int spaceInt = (int)Math.Floor(spaceDouble);
+                for (int j = 0; j < spaceInt; j++)
+                {
+                    Console.Write(" ");
+                }
+                // draw stars
+                double starDouble =  (Math.Abs(i - (number - 1) / 2.0))* (-2) + number;
+                int starInt = (int)Math.Floor(starDouble);
+                for (int j = 0; j < starInt; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\r\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
