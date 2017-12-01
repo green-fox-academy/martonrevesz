d4befcdaa954a9acd12cdffb643b49940130292d
Marton Revesz <martonrevesz@gmail.com>
2017-12-01 13:49:55 +0100

    feat(JosephusProblem): create

---
+++ week-02/day-05/JosephusProblem/JosephusProblem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace JosephusProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 3;
+            Console.WriteLine(FindJosephus(number));
+            Console.ReadLine();
+        }
+
+        static int FindJosephus (int number)
+        {
+            int i = 0;
+            int goodExponent = 0;
+            while (Math.Pow(2,i) <= number)
+            {
+                i++;
+            }
+            i--;
+
+            int remainder = number % (int) (Math.Pow(2,i));
+            int goodPlace = remainder * 2 + 1;
+            return goodPlace ;
+        }
+    }
+}
