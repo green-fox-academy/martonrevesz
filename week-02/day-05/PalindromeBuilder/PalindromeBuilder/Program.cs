81f8855f4015bd9cde602b34b1293a9c97a459a7
Marton Revesz <martonrevesz@gmail.com>
2017-12-01 10:36:25 +0100

    feat(PalindromeBuilder): create

---
+++ week-02/day-05/PalindromeBuilder/PalindromeBuilder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PalindromeBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "jhkjh5355";
+            Console.WriteLine(ToPalindrome(input));
+            Console.ReadLine();
+        }
+
+        static string ToPalindrome(string input)
+        {
+            char[] cArray = input.ToCharArray();
+            string reverse = string.Empty;
+
+            for (int i = 0; i < cArray.Length; i++)
+            {
+                reverse += cArray[cArray.Length - (i + 1)];
+            }
+            string palindrome = input + reverse;
+            return palindrome;
+        }
+    }
+}
