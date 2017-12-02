ace3d2d0f149af0e2387e79eb8156f09b8d4454b
Marton Revesz <martonrevesz@gmail.com>
2017-12-02 16:44:51 +0100

    feat(FindTheSubstriing): create

---
+++ week-02/day-04/FindTheSubstring/FindTheSubstring/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FindTheSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindTheSubstring("student of the green fox academy", "fox"));
+            Console.ReadLine();
+        }
+
+        static int FindTheSubstring(string container, string sub)
+        {
+            for (int i = 0; i < container.Length - (sub.Length - 1); i++)
+            {
+                if (container.Substring(i, sub.Length) == sub)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
