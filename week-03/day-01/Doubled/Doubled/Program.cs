a6b7ac8c20f5d1a52167339caf98a89fa3fa372b
Marton Revesz <martonrevesz@gmail.com>
2017-12-04 19:24:11 +0100

    feat(Doubled): write the result into a txt-file

--- week-03/day-01/Doubled/Doubled/Program.cs
+++ week-03/day-01/Doubled/Doubled/Program.cs
@@ -8,21 +8,36 @@ namespace Doubled
         static void Main(string[] args)
         {
             string path = @"C:\Users\Hajnal és Marci\greenfox\martonrevesz\week-03\day-01\duplicated-chars.txt";
-            DecryptDuplicated(path);
+            string path2 = @"C:\Users\Hajnal és Marci\greenfox\martonrevesz\week-03\day-01\duplicated-chars-copy.txt";
+            DecryptDuplicated(path, path2);
             Console.ReadLine();
         }
-        static void DecryptDuplicated(string path)
+        static void DecryptDuplicated(string path, string path2)
         {
-            string[] array = File.ReadAllLines(path);
-            foreach (string line in array)
+            var sw = new StreamWriter(path2);
+            try
             {
-                char[] charArray = line.ToCharArray();
-                for (int i = 0; i < charArray.Length; i += 2)
+                string[] array = File.ReadAllLines(path);
+                foreach (string line in array)
                 {
-                    Console.Write(charArray[i]);
+                    char[] charArray = line.ToCharArray();
+                    for (int i = 0; i < charArray.Length; i += 2)
+                    {
+                        Console.Write(charArray[i]);
+                        sw.Write(charArray[i]);
+                    }
+                    Console.WriteLine("\n");
+                    sw.Write("\n");
                 }
-                Console.WriteLine("\n");
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                sw.Close();
+            }            
         }
     }
 }
