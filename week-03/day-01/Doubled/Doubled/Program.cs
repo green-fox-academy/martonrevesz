5dc6283f4c6d007b23798b27bdf3b169808c743b
Marton Revesz <martonrevesz@gmail.com>
2017-12-04 14:54:10 +0100

    feat(Doubled): create

---
+++ week-03/day-01/Doubled/Doubled/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Doubled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Hajnal és Marci\greenfox\martonrevesz\week-03\day-01\duplicated-chars.txt";
+            DecryptDuplicated(path);
+            Console.ReadLine();
+        }
+        static void DecryptDuplicated(string path)
+        {
+            string[] array = File.ReadAllLines(path);
+            foreach (string line in array)
+            {
+                char[] charArray = line.ToCharArray();
+                for (int i = 0; i < charArray.Length; i += 2)
+                {
+                    Console.Write(charArray[i]);
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
