a49179564401ef80000226602a0762cd5327f482
Marton Revesz <martonrevesz@gmail.com>
2017-12-04 11:54:34 +0100

    feat(PrintEachLine): create

---
+++ week-03/day-01/PrintEachLine/PrintEachLine/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace PrintEachLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] array = File.ReadAllLines(@"C:\Users\Hajnal és Marci\greenfox\martonrevesz\week-03\day-01\my-file.txt");
+                foreach (string line in array)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
