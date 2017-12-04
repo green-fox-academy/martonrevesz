84373166ff56d9cbd9d83ee181f79f0b845e709e
Marton Revesz <martonrevesz@gmail.com>
2017-12-04 13:37:41 +0100

    feat(MySingleLine): create

---
+++ week-03/day-01/WriteSingleLine/WriteSingleLine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Hajnal és Marci\greenfox\martonrevesz\week-03\day-01\my-file.txt";
+            string myName = "Coffee";
+            StreamWriter sw = new StreamWriter(path, true);
+            try
+            {
+                sw.WriteLine(myName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to write file: my-file.txt");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                sw.Close();
+            }
+            Console.ReadLine();
+        }
+    }
+}
