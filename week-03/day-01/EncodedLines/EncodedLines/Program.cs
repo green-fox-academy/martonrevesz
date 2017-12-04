e9805becc3b6d7db19d57171ed578178eb72d4e1
Marton Revesz <martonrevesz@gmail.com>
2017-12-04 17:29:29 +0100

    feat(EncodedLines): create

---
+++ week-03/day-01/EncodedLines/EncodedLines/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace EncodedLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BreakCaesar(@"C:\Users\Hajnal és Marci\greenfox\martonrevesz\week-03\day-01\encoded-lines.txt");
+            Console.ReadLine();
+        }
+
+        static void BreakCaesar(string path)
+        {
+            string[] lines = null;
+            int caesarNumber = 1;
+
+            try
+            {
+                lines = File.ReadAllLines(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            foreach (string line in lines)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i].Equals(' '))
+                    {
+                        Console.Write(' ');
+                    }
+                    else
+                    {
+                        int myChar = Convert.ToInt32(line[i]);
+                        Console.Write(Convert.ToChar((myChar - caesarNumber)));
+                    }
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
