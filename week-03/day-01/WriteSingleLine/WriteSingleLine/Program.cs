cdc76e5191fe6ea5ef567a662e9511e44313ad66
Marton Revesz <martonrevesz@gmail.com>
2017-12-04 16:49:22 +0100

    feat(WriteSingleLine): enable user input

--- week-03/day-01/WriteSingleLine/WriteSingleLine/Program.cs
+++ week-03/day-01/WriteSingleLine/WriteSingleLine/Program.cs
@@ -7,12 +7,24 @@ namespace WriteSingleLine
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\Hajnal és Marci\greenfox\martonrevesz\week-03\day-01\my-file.txt";
-            string myName = "Coffee";
-            StreamWriter sw = new StreamWriter(path, true);
+            StreamWriter sw = null;
             try
             {
+                Console.WriteLine("Please enter the only valid filepath without quotes: ");
+                //"C:\Users\Hajnal és Marci\greenfox\martonrevesz\week-03\day-01\my-file.txt"
+                string path = @Console.ReadLine();
+                if (!path.Equals(@"C:\Users\Hajnal és Marci\greenfox\martonrevesz\week-03\day-01\my-file.txt"))
+                {
+                    throw new MySpecialException();
+                }
+                Console.WriteLine("Please enter your name: ");
+                string myName = Console.ReadLine();
+                sw = new StreamWriter(path, true);
                 sw.WriteLine(myName);
+            }
+            catch (MySpecialException e)
+            {
+                Console.WriteLine(e.Message);
             }
             catch (Exception e)
             {
@@ -21,9 +33,20 @@ namespace WriteSingleLine
             }
             finally
             {
-                sw.Close();
+                if (sw != null)
+                {
+                    sw.Close();
+                }
+
             }
             Console.ReadLine();
+        }
+    }
+    class MySpecialException : Exception
+    {
+        public MySpecialException()
+        {
+            Console.WriteLine("Invalid file name!!!");
         }
     }
 }
