68bfb4f5f0d1a98615ba51e409651db7136c87ce
Marton Revesz <martonrevesz@gmail.com>
2017-12-04 13:56:47 +0100

    feat(WriteMultipleLines): create

---
+++ week-03/day-01/WriteMultipleLines/WriteMultipleLines/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace WriteMultipleLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Hajnal és Marci\greenfox\martonrevesz\week-03\day-01\my-file.txt";
+            string word = "apple";
+            int number = 5;
+            int counter = 0;
+            var sw = new StreamWriter(path, true);
+            try
+            {
+                while (counter < number)
+                {
+                    sw.WriteLine(word);
+                    counter++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                sw.Dispose();
+            }
+        }
+    }
+}
