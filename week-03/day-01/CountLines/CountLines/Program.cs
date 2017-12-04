9383274d3aaeae563602a63c7d91e93a18f436d6
Marton Revesz <martonrevesz@gmail.com>
2017-12-04 12:09:57 +0100

    feat(CountLines): create

---
+++ week-03/day-01/CountLines/CountLines/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Hajnal és Marci\greenfox\martonrevesz\week-03\day-01\my-file.txt";
+            Console.WriteLine(CountLines(path));
+            Console.ReadLine();
+        }
+
+        static int CountLines(string path)
+        {
+            try
+            {
+                int count = 0;
+                var sb = new StreamReader(path);
+                while (sb.ReadLine() != null)
+                {
+                    count++;
+                }
+                return count;
+            }
+            catch (FileNotFoundException e)
+            {
+                return 0;
+            }
+        }
+    }
+}
