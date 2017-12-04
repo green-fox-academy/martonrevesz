0a14d6e8d206b9264d9dd84c005423b8b0cbb26a
Marton Revesz <martonrevesz@gmail.com>
2017-12-04 15:24:54 +0100

    feat(Reversed): create

---
+++ week-03/day-01/Reversed/Reversed/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Reversed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Hajnal és Marci\greenfox\martonrevesz\week-03\day-01\reversed-lines.txt";
+            DecryptReverse(path);
+            Console.ReadLine();
+        }
+        static void DecryptReverse(string path)
+        {
+            string[] myArray = File.ReadAllLines(path);
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                for (int j = 0; j < myArray[i].Length; j++)
+                {
+                    Console.Write(myArray[i][myArray[i].Length - (j + 1)]);
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
