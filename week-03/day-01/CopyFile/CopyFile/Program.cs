2e1d0f5bab9cc584922d81ca1181b6c284e0d909
Marton Revesz <martonrevesz@gmail.com>
2017-12-04 14:23:32 +0100

    feat(CopyFile): create

---
+++ week-03/day-01/CopyFile/CopyFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName1 = @"C:\Users\Hajnal és Marci\greenfox\martonrevesz\week-03\day-01\my-file.txt";
+            string fileName2 = @"C:\Users\Hajnal és Marci\greenfox\martonrevesz\week-03\day-01\my-file-copy.txt";
+            CopyFile(fileName1, fileName2);
+        }
+        static bool CopyFile(string file1, string file2)
+        {
+            try
+            {
+                string[] array = File.ReadAllLines(file1);
+                File.WriteAllLines(file2, array);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+    } 
+}
