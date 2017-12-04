cc31d7f34f8571dd5534327ed8ecefe57cb40f3d
Marton Revesz <martonrevesz@gmail.com>
2017-12-04 11:37:25 +0100

    feat(DivideByZero): create

---
+++ week-03/day-01/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            DivideTenBy(a);
+            Console.ReadLine();
+        }
+        static void DivideTenBy(int number)
+        {
+            try
+            {
+                Console.WriteLine(10 / number);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
