be2f4ee6fdf94ac713999881cd08ba8fc134fbf6
Marton Revesz <martonrevesz@gmail.com>
2017-12-06 10:58:34 +0100

    feat(ColoredBox): create

---
+++ week-03/day-03/ColoredBox/ColoredBox/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace ColoredBox
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // draw a box that has different colored lines on each edge.
+            DrawColoredBox(foxDraw);
+        }
+
+        public static void DrawColoredBox(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Green); 
+            foxDraw.DrawLine(10, 10, 200, 10);
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(200, 10, 200, 100);
+            foxDraw.StrokeColor(Colors.Blue);
+            foxDraw.DrawLine(200, 100, 10, 100);
+            foxDraw.StrokeColor(Colors.Yellow);
+            foxDraw.DrawLine(10, 100, 10, 10);
+        }
+    }
+}
+
