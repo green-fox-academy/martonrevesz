f4382375a46579061962d6d5dab12deb0a499e41
Marton Revesz <martonrevesz@gmail.com>
2017-12-06 14:16:26 +0100

    feat(PurpleSteps): create

---
+++ week-03/day-03/PurpleSteps/PurpleSteps/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace PurpleSteps
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            PurpleSteps(foxDraw);
+        }
+        static void PurpleSteps(FoxDraw foxDraw)
+        {
+            foxDraw.FillColor(Colors.Purple);
+            foxDraw.StrokeColor(Colors.Black);
+            for (int i = 0; i < 30; i++)
+            {
+                foxDraw.DrawRectangle(10 + 15 * i, 10 + 15 * i , 15, 15);
+            }
+        }
+    }
+}
