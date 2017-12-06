d5128d7d80806a9f4e85dc53c38a13ddeb614098
Marton Revesz <martonrevesz@gmail.com>
2017-12-06 12:36:20 +0100

    feat(FourRectangles): create

---
+++ week-03/day-03/FourRectangles/FourRectangles/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace FourRectangles
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawRectangle(foxDraw);
+        }
+        static void DrawRectangle(FoxDraw foxDraw)
+        {
+            foxDraw.FillColor(Colors.Green);
+            foxDraw.DrawRectangle(50, 50, 60, 60);
+
+            foxDraw.FillColor(Colors.Red);
+            foxDraw.DrawRectangle(110, 110, 70, 70);
+
+            foxDraw.FillColor(Colors.Blue);
+            foxDraw.DrawRectangle(180, 180, 80, 80);
+
+            foxDraw.FillColor(Colors.Yellow);
+            foxDraw.DrawRectangle(260, 260, 30, 30);
+        }
+    }
+}
