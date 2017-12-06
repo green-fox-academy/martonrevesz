26f09acec2345be25f1d0ea188259ce9d3791e00
Marton Revesz <martonrevesz@gmail.com>
2017-12-06 14:58:15 +0100

    feat(CheckerBoard): create

---
+++ week-03/day-03/CheckerBoard/CheckerBoard/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace CheckerBoard
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawChessBoard(foxDraw);
+        }
+        static void DrawChessBoard(FoxDraw foxDraw)
+        {
+            for (int i = 0; i < 18; i++)
+            {
+                for (int j = 0; j < 18; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        foxDraw.FillColor(Colors.Black);
+                    }
+                    else
+                    {
+                        foxDraw.FillColor(Colors.White);
+                    }
+                    foxDraw.DrawRectangle(10 + 15 * j, 10 + 15 * i , 15 , 15);
+                }
+            }
+        }
+    }
+}
