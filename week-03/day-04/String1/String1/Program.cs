1e3fdb468fc78d6565aa069c04f776d20b58f178
Marton Revesz <martonrevesz@gmail.com>
2017-12-07 12:13:26 +0100

    feat(String1): create

---
+++ week-03/day-04/String1/String1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReplaceXToY("xgxXw yda"));
+            Console.ReadLine();
+        }
+        static string ReplaceXToY(string text)
+        {
+            if (text.Length == 0)
+            {
+                return text;
+            }
+            if (text[0] == 'x')
+            {
+                text = "y" + text.Substring(1);
+            }
+            return text[0] + ReplaceXToY(text.Substring(1));
+        }
+    }
+}
