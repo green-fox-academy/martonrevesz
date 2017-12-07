413c0dcba7db1bd9a3eaba8c064291dee764ea2a
Marton Revesz <martonrevesz@gmail.com>
2017-12-07 12:27:49 +0100

    feat(String3): create

---
+++ week-03/day-04/String3/String3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToAsterix("happyDayAtGreenFox"));
+            Console.ReadLine();
+        }
+        static string ToAsterix(string text)
+        {
+            if (text.Length == 1)
+            {
+                return text;
+            }
+            text = text[0] + "*" + text.Substring(1);
+            return text.Substring(0, 2) + ToAsterix(text.Substring(2));
+        }
+    }
+}
