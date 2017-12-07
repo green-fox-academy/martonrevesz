3743f619305d67c1ac97d6b004622230f79930c3
Marton Revesz <martonrevesz@gmail.com>
2017-12-07 13:46:38 +0100

    feat(Fibonacci): create

---
+++ week-03/day-04/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindNthFibonacci(8));
+            Console.ReadLine();
+        }
+        static int FindNthFibonacci(int n)
+        {
+            if (n == 1 | n == 0)
+            {
+                return 1;
+            }
+            return FindNthFibonacci(n - 1) + FindNthFibonacci(n - 2);
+        }
+    }
+}
