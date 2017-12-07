cf060fa572ab36837345acffee0753c3f37a61c8
Marton Revesz <martonrevesz@gmail.com>
2017-12-07 10:53:03 +0100

    feat(AddN): create

---
+++ week-03/day-04/AddN/AddN/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddNumbersToN(9));
+            Console.ReadLine();
+        }
+        static int AddNumbersToN(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            return n + AddNumbersToN(n - 1);
+        }
+    }
+}
