5421b3b0b8b23b0d8506ad02a4eb0295283b8aed
Marton Revesz <martonrevesz@gmail.com>
2017-12-07 11:02:25 +0100

    feat(SumDigit): create

---
+++ week-03/day-04/SumDigit/SumDigit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumDigit(2017));
+            Console.ReadLine();
+        }
+
+        static int SumDigit(int n)
+        {
+            if (n < 10)
+            {
+                return n;
+            }
+            return (n % 10) + SumDigit(n / 10);          
+        }
+    }
+}
