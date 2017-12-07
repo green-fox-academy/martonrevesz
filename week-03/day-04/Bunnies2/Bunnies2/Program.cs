9d3793a2f0b791a8d521bd576ad7db248c277c26
Marton Revesz <martonrevesz@gmail.com>
2017-12-07 11:47:02 +0100

    feat(Bunnies2): create

---
+++ week-03/day-04/Bunnies2/Bunnies2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunnies2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountBunnies(19));
+            Console.ReadLine();
+        }
+
+        static int CountBunnies(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n % 2 == 0)
+            {
+                return 3 + CountBunnies(n - 1);
+            }
+            return 2 + CountBunnies(n - 1);
+        }
+    }
+}
