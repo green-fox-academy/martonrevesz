9b54be95d8b7527deced6a7160e146c3fdd6e086
Marton Revesz <martonrevesz@gmail.com>
2017-12-07 11:12:06 +0100

    feat(Power): create

---
+++ week-03/day-04/Power/Power/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseNumber = 3;
+            int exponent = 6;
+            Console.WriteLine(Power(baseNumber, exponent));
+            Console.ReadLine();
+        }
+
+        static int Power(int baseNumber, int exponent)
+        {
+            if (exponent == 0)
+            {
+                return 1;
+            }
+            return baseNumber * Power(baseNumber, exponent - 1);
+        }
+    }
+}
