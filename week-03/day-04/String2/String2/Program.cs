6f9f1726206153dce3ea78a2ec90077a693f9860
Marton Revesz <martonrevesz@gmail.com>
2017-12-07 12:18:46 +0100

    feat(String2): create

---
+++ week-03/day-04/String2/String2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveX("xtyxikXy"));
+            Console.ReadLine();
+        }
+
+        static string RemoveX(string text)
+        {
+            if (text.Length == 0)
+            {
+                return text;
+            }
+            if (text[0] == 'x')
+            {
+                text = text.Substring(1);
+            }
+            return text[0] + RemoveX(text.Substring(1));
+        }
+    }
+}
