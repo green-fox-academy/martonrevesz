6ad476997bc7bbe859ead98f59ac51446d62d13d
Marton Revesz <martonrevesz@gmail.com>
2017-12-07 10:41:32 +0100

    feat(Counter): create

---
+++ week-03/day-04/Counter/Counter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountDown(15);
+            Console.ReadLine();
+        }
+
+        static void CountDown(int n)
+        {
+            Console.WriteLine(n);
+            if (n > 0)
+            {
+                CountDown(n - 1);
+            }
+            
+        }
+    }
+}
