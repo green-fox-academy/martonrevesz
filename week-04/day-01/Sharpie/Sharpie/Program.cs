bffacb69a378e5815b082a35024afc2af5a35976
Marton Revesz <martonrevesz@gmail.com>
2017-12-11 16:01:40 +0100

    feat(Sharpie): create

---
+++ week-04/day-01/Sharpie/Sharpie/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sharpie1 = new Sharpie("red", 2.3);
+            var sharpie2 = new Sharpie("blue", 1.8);
+            sharpie1.Use();
+            sharpie1.Use();
+            sharpie2.Use();
+            Console.WriteLine(sharpie1.InkAmount + ", " + sharpie1.Color);
+            Console.WriteLine(sharpie2.InkAmount + ", " + sharpie2.Width);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-04/day-01/Sharpie/Sharpie/Sharpie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpie
+{
+    class Sharpie
+    {
+        public string Color { get; set; }
+        public double Width { get; set; }
+        public double InkAmount { get; set; } = 100;
+
+        public Sharpie(string color, double width)
+        {
+            Color = color;
+            Width = width;
+        }
+
+        public void Use()
+        {
+            InkAmount--;
+        }
+    }
+}
