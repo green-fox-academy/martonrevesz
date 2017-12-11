6454f70b373dff6f82dce2ad46698a1b47822d4a
Marton Revesz <martonrevesz@gmail.com>
2017-12-11 16:38:03 +0100

    feat(Counter): create

---
+++ week-04/day-01/Counter/Counter/Counter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Counter
+    {
+        public int CurrentValue { get; set; }
+
+        public Counter()
+        {
+            CurrentValue = 0;
+        }
+
+        public Counter(int currentValue)
+        {
+            CurrentValue = currentValue;
+        }
+
+        public void Add(int number)
+        {
+            CurrentValue += number;
+        }
+
+        public void Add()
+        {
+            CurrentValue++;
+        }
+
+        public string Get()
+        {
+            return CurrentValue.ToString();
+        }
+
+        public void Reset()
+        {
+            CurrentValue = 0;
+        }
+    }
+}
---
+++ week-04/day-01/Counter/Counter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCounter = new Counter(50);
+            myCounter.Add(5);
+            myCounter.Add();
+            Console.WriteLine(myCounter.Get());
+            myCounter.Reset();
+            Console.WriteLine(myCounter.Get());
+            Console.ReadLine();
+        }
+    }
+}
