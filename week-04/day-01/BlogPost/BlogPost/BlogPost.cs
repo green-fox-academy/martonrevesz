ecc0a95f50da4c07e23dc3f80dfb10ea2a381100
Marton Revesz <martonrevesz@gmail.com>
2017-12-11 12:20:24 +0100

    feat(BlogPost): create

---
+++ week-04/day-01/BlogPost/BlogPost/BlogPost.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPost
+{
+    class BlogPost
+    {
+        public string AuthorName { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string PublicationDate { get; set; }
+
+        public BlogPost(string authorName, string title, string text, string publicationDate)
+        {
+            AuthorName = authorName;
+            Title = title;
+            Text = text;
+            PublicationDate = publicationDate;
+        }
+
+        public override string ToString()
+        {
+            return "[AuthorName] = " + AuthorName + " [Title] = " + Title + " [Text] = " + Text + " [PublicationDate] = " + PublicationDate;
+        }
+    }
+}
---
+++ week-04/day-01/BlogPost/BlogPost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var blogPost1 = new BlogPost("John Doe", "Lorem ipsum", "Lorem ipsum dolor sit amet.", "2000.05.04.");
+            var blogPost2 = new BlogPost("Tim Urban", "Wait but why", "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10.");
+            var blogPost3 = new BlogPost("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump", "Daniel Hanley, a cybersecurity engineer at IBM.", "2017.03.28.");
+
+            Console.WriteLine(blogPost3.ToString());
+            Console.ReadLine();
+        }
+    }
+}
