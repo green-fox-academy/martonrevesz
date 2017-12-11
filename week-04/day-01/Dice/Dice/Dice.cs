40d2d6e0195bcd2c5967bbabe434e9b43e9d8134
Marton Revesz <martonrevesz@gmail.com>
2017-12-11 13:13:45 +0100

    feat(Dice): create

---
+++ week-04/day-01/Dice/Dice/Dice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Dice
+    {
+        private int currentNumber;
+        Random random = new Random();
+
+        public int GetCurrentNumber()
+        {
+            return currentNumber;
+        }
+
+        public void Roll()
+        {
+            currentNumber = random.Next(1, 7);
+        }
+
+        public void ReRoll()
+        {
+            currentNumber = random.Next(1, 7);
+        }
+    }
+}
---
+++ week-04/day-01/Dice/Dice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Dice>();
+            for (int i = 0; i < 6; i++)
+            {
+                list.Add(new Dice());
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                list[i].Roll();
+                if (list[i].GetCurrentNumber() != 6)
+                {
+                    list[i].ReRoll();
+                }
+                Console.WriteLine(list[i].GetCurrentNumber());
+            }
+            Console.ReadLine();
+        }
+    }
+}
