f463d21fcf596185191c0450ca3acad20e21b38f
Marton Revesz <martonrevesz@gmail.com>
2017-12-11 15:44:25 +0100

    feat(Animal): create

---
+++ week-04/day-01/Animal/Animal/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Animal
+    {
+        public int HungerValue { get; set; }
+        public int ThirstValue { get; set; }
+
+        public Animal()
+        {
+            HungerValue = 50;
+            ThirstValue = 50;
+        }
+
+        public void Eat()
+        {
+            HungerValue--;
+        }
+
+        public void Drink()
+        {
+            ThirstValue--;
+        }
+
+        public void Play()
+        {
+            HungerValue++;
+            ThirstValue++;
+        }
+    }
+}
---
+++ week-04/day-01/Animal/Animal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tiger = new Animal();
+            var zebra = new Animal();
+            tiger.Eat();
+            tiger.Eat();
+            tiger.Drink();
+            tiger.Play();
+            zebra.Drink();
+            Console.WriteLine(tiger.HungerValue + ", " +  tiger.ThirstValue + "\n");
+            Console.WriteLine(zebra.HungerValue + ", " +  zebra.ThirstValue + "\n");
+            Console.ReadLine();
+        }
+    }
+}
