47f08cbf94b315a170386838e0eca05eedf9a0b0
Marton Revesz <martonrevesz@gmail.com>
2017-12-11 20:52:08 +0100

    feat(Animal): create Farm class with methods Breed and Slaughter

---
+++ week-04/day-01/Animal/Animal/Farm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm
+{
+    class Farm
+    {
+        public List<Animal> AnimalList { get; set; }
+        public int FreePlaces { get; set; }
+
+        public Farm(List<Animal> animalList, int freePlaces)
+        {
+            AnimalList = animalList;
+            FreePlaces = freePlaces - animalList.Count;
+        }
+
+        public void Breed()
+        {
+            if (FreePlaces > 0)
+            {
+                AnimalList.Add(new Animal());
+                FreePlaces--;
+            }
+        }
+        public void Slaughter()
+        {
+            var theLeastHungry = AnimalList[0];
+            foreach (Animal item in AnimalList)
+            {
+                if (theLeastHungry.HungerValue > item.HungerValue)
+                {
+                    theLeastHungry = item;
+                }
+            }
+            AnimalList.Remove(theLeastHungry);
+            FreePlaces++;
+        }
+    }
+}
--- week-04/day-01/Animal/Animal/Program.cs
+++ week-04/day-01/Animal/Animal/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Animal
+namespace Farm
 {
     class Program
     {
@@ -12,13 +12,25 @@ namespace Animal
         {
             var tiger = new Animal();
             var zebra = new Animal();
+            var lion = new Animal();
+            var farm = new Farm(new List<Animal>() { tiger, lion, zebra }, 3);
             tiger.Eat();
             tiger.Eat();
             tiger.Drink();
             tiger.Play();
             zebra.Drink();
-            Console.WriteLine(tiger.HungerValue + ", " +  tiger.ThirstValue + "\n");
-            Console.WriteLine(zebra.HungerValue + ", " +  zebra.ThirstValue + "\n");
+            lion.Eat();
+            lion.Eat();
+            Console.WriteLine("tiger: " + tiger.HungerValue + ", " + tiger.ThirstValue + "\n");
+            Console.WriteLine("zebra: " + zebra.HungerValue + ", " + zebra.ThirstValue + "\n");
+            Console.WriteLine("lion: " + lion.HungerValue + ", " + lion.ThirstValue + "\n");
+            farm.Breed();
+            farm.Breed();
+            farm.Slaughter();
+            for (int i = 0; i < farm.AnimalList.Count; i++)
+            {
+                Console.WriteLine(farm.AnimalList[i].HungerValue);
+            }
             Console.ReadLine();
         }
     }
