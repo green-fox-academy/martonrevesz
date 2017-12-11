82990620d2c1d38d09df044d9d5da67d270c5423
Marton Revesz <martonrevesz@gmail.com>
2017-12-11 14:59:53 +0100

    feat(FleetOfThings): create

---
+++ week-04/day-01/FleetOfThings/FleetOfThings/Fleet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetOfThings
+{
+    class Fleet
+    {
+        private List<Thing> Things;
+
+        public Fleet()
+        {
+            Things = new List<Thing>();
+        }
+
+        public void Add(Thing thing)
+        {
+            Things.Add(thing);
+        }
+
+        public void PrintFleet()
+        {
+            for (int i = 0; i < Things.Count; i++)
+            {
+                var sb = new StringBuilder();
+                sb.Append((i + 1) + ". [");
+                if (Things[i].GetCompleted())
+                {
+                    sb.Append("X");
+                }
+                else
+                {
+                    sb.Append(" ");
+                }
+                sb.Append("] " + Things[i].GetName());
+                Console.WriteLine(sb.ToString());
+            }
+        }
+    }
+}
---
+++ week-04/day-01/FleetOfThings/FleetOfThings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetOfThings
+{
+    class FleetOfThings
+    {
+        static void Main(string[] args)
+        {
+            var fleet = new Fleet();
+            var thing0 = new Thing("Get milk");
+            var thing1 = new Thing("Remove the obstacles");
+            var thing2 = new Thing("Stand up");
+            var thing3 = new Thing("Eat lunch");
+            fleet.Add(thing0);
+            fleet.Add(thing1);
+            fleet.Add(thing2);
+            fleet.Add(thing3);
+            thing2.Complete();
+            thing3.Complete();
+            fleet.PrintFleet();
+            Console.ReadLine();
+        }
+    }
+}
