0b7814f55fbaf0f05291d82e779a789a54944f1e
Marton Revesz <martonrevesz@gmail.com>
2017-12-12 16:45:21 +0100

    feat(Aircrafts): create Carrier class, AddAircraft and Fill methods

---
+++ week-04/day-02/Aircrafts/Aircrafts/Carrier.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aircrafts
+{
+    class Carrier
+    {
+        public List<Aircraft> AircraftList { get; set; }
+        public int AmmoStore { get; set; }
+        public int HP { get; set; }
+
+        public Carrier(int ammoStore, int hp)
+        {
+            AircraftList = new List<Aircraft>();
+            AmmoStore = ammoStore;
+            HP = hp;
+        }
+
+        public void AddAircraft(Aircraft aircraft)
+        {
+            if (!AircraftList.Contains(aircraft))
+            {
+                AircraftList.Add(aircraft);
+            }
+        }
+
+        public void Fill()
+        {
+            try
+            {
+                if (AmmoStore == 0)
+                {
+                    throw new Exception();
+                }
+                foreach (Aircraft aircraft in AircraftList)
+                {
+                    if (aircraft.GetAircraftType() == "F35")
+                    {
+                        AmmoStore = aircraft.Refill(AmmoStore);
+                    }
+                }
+                foreach (Aircraft aircraft in AircraftList)
+                {
+                    if (aircraft.GetAircraftType() == "F16")
+                    {
+                        AmmoStore = aircraft.Refill(AmmoStore);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- week-04/day-02/Aircrafts/Aircrafts/Program.cs
+++ week-04/day-02/Aircrafts/Aircrafts/Program.cs
@@ -10,11 +10,20 @@ namespace Aircrafts
     {
         static void Main(string[] args)
         {
-            var f16first = new F35();
-            Console.WriteLine(f16first.Refill(16).ToString());
-            Console.WriteLine(f16first.Fight());
-            Console.WriteLine(f16first.GetStatus());
-            Console.ReadLine();
+                var f16First = new F16();
+                var f16Second = new F16();
+                var f35First = new F35();
+                var carrier1 = new Carrier(0, 300);
+                carrier1.AddAircraft(f16First);
+                carrier1.AddAircraft(f16Second);
+                carrier1.AddAircraft(f35First);
+                carrier1.AddAircraft(f35First);
+                carrier1.Fill();
+                Console.WriteLine(f16First.GetStatus());
+                Console.WriteLine(f16Second.GetStatus());
+                Console.WriteLine(f35First.GetStatus());
+                Console.WriteLine(carrier1.AmmoStore);
+                Console.ReadLine();
         }
     }
 }
