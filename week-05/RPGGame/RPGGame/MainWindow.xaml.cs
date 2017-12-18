f688bd9df98e7ffbffc01ebb2cfa44e0ea8dd6e9
Marton Revesz <martonrevesz@gmail.com>
2017-12-18 15:25:47 +0100

    feat(Limits to hero movement):

--- week-05/RPGGame/RPGGame/MainWindow.xaml.cs
+++ week-05/RPGGame/RPGGame/MainWindow.xaml.cs
@@ -18,13 +18,15 @@ namespace RPGGame
     public partial class MainWindow : Window
     {
         public FoxDraw FoxDraw;
+        Character hero;
+        Map map;
 
         public MainWindow()
         {
             InitializeComponent();
             FoxDraw = new FoxDraw(canvas);
-            var map = new Map(FoxDraw);
-            var hero = new Character("hero", "./Assets/hero-down.png");
+            map = new Map(FoxDraw);
+            hero = new Character("hero", "./Assets/hero-down.png");
             map.DrawMap();
             map.DrawCharacter(hero);
             
@@ -32,29 +34,33 @@ namespace RPGGame
 
         private void WindowsKeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Right)
+            if (e.Key == Key.Right && (hero.TileNumber % 10 != 9) && !map.WallTiles.Contains(hero.TileNumber + 1))
             {
                 double x = FoxDraw.GetLeft(FoxDraw.Tiles[110]) + 50;
                 double y = FoxDraw.GetTop(FoxDraw.Tiles[110]);
-                FoxDraw.SetPosition(FoxDraw.Tiles[110], x, y);                  
+                FoxDraw.SetPosition(FoxDraw.Tiles[110], x, y);
+                hero.TileNumber += 1;
             }
-            if (e.Key == Key.Left)
+            if (e.Key == Key.Left && (hero.TileNumber % 10 != 0) && !map.WallTiles.Contains(hero.TileNumber - 1))
             {
                 double x = FoxDraw.GetLeft(FoxDraw.Tiles[110]) - 50;
                 double y = FoxDraw.GetTop(FoxDraw.Tiles[110]);
                 FoxDraw.SetPosition(FoxDraw.Tiles[110], x, y);
+                hero.TileNumber -= 1;
             }
-            if (e.Key == Key.Up)
+            if (e.Key == Key.Up && (hero.TileNumber / 10 != 0) && !map.WallTiles.Contains(hero.TileNumber - 10))
             {
                 double x = FoxDraw.GetLeft(FoxDraw.Tiles[110]);
                 double y = FoxDraw.GetTop(FoxDraw.Tiles[110]) - 50;
                 FoxDraw.SetPosition(FoxDraw.Tiles[110], x, y);
+                hero.TileNumber -= 10;
             }
-            if (e.Key == Key.Down)
+            if (e.Key == Key.Down && (hero.TileNumber / 10 != 10) && !map.WallTiles.Contains(hero.TileNumber + 10))
             {
                 double x = FoxDraw.GetLeft(FoxDraw.Tiles[110]);
                 double y = FoxDraw.GetTop(FoxDraw.Tiles[110]) + 50;
                 FoxDraw.SetPosition(FoxDraw.Tiles[110], x, y);
+                hero.TileNumber += 10;
             }
         }
     }
