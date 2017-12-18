f447be16893e94b8894d105f23deacf08a697112
Marton Revesz <martonrevesz@gmail.com>
2017-12-18 13:09:51 +0100

    feat(Project): create

---
+++ week-05/RPGGame/RPGGame/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPGGame
+{
+    public partial class MainWindow : Window
+    {
+        private FoxDraw FoxDraw;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FoxDraw = new FoxDraw(canvas);
+            FoxDraw.AddImage("./Assets/boss.png", 0, 0);
+        }
+
+        private void WindowsKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F)
+            {
+                double x = FoxDraw.GetLeft(FoxDraw.Tiles[0]) + 10;
+                double y = FoxDraw.GetTop(FoxDraw.Tiles[0]) + 10;
+                FoxDraw.SetPosition(FoxDraw.Tiles[0], x, y);
+            }
+        }
+    }
+}
