073b07d437a714eb3323a9e66570e526168a3fae
Marton Revesz <martonrevesz@gmail.com>
2017-12-20 17:39:22 +0100

    feat(FavouriteAnimals): create

---
+++ week-05/PallidaExamFavouriteAnimals/PallidaExamFavouriteAnimals/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PallidaExamFavouriteAnimals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "favourites.txt";
+            
+            if (args.Length == 0)
+            {
+                Console.WriteLine("C# FavouriteAnimals [animal] [animal]");
+            }
+            else
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    string[] currentContent = File.ReadAllLines(fileName);
+                    var streamWriter = new StreamWriter(fileName, true);                   
+                    var testList = currentContent.ToList();
+                    if (!testList.Contains(args[i]))
+                    {
+                        streamWriter.WriteLine(args[i]);
+                        
+                    }
+                    streamWriter.Close();
+
+                }
+            }
+        }
+    }
+}
