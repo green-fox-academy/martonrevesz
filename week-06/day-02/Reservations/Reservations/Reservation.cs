7763fac27fe08df1e00d4ee6c284242ebfb73cbf
Marton Revesz <martonrevesz@gmail.com>
2018-01-02 16:14:55 +0100

    feat(Reservations): create

---
+++ week-06/day-02/Reservations/Reservations/Reservation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservations
+{
+    class Reservation : IReservation
+    {
+        Random random = new Random();
+
+        public string GetCodeBooking()
+        {
+            var options = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            var code = string.Empty;
+            for (int i = 0; i < 8; i++)
+            {
+                code += options[random.Next(26)];
+            }
+            return code;
+        }
+
+        public string GetDowBooking()
+        {
+            string[] days = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN" };
+            return days[random.Next(7)];
+        }
+    }
+}
---
+++ week-06/day-02/Reservations/Reservations/ReservationApp.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservations
+{
+    class ReservationApp
+    {
+        static void Main(string[] args)
+        {
+            var reservation = new Reservation();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Booking# {reservation.GetCodeBooking()} for {reservation.GetDowBooking()}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
