2c04fd44aa013fc2d1717bbdbbf65911838d5f2c
Marton Revesz <martonrevesz@gmail.com>
2018-01-03 13:50:39 +0100

    feat(Excercise4): create

---
+++ week-06/day-03/Linq/Excercise4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new[] { 3, 9, 2, 8, 6, 5 };
+            numbers.Where(n => n * n > 20).ToList().ForEach(n => Console.WriteLine(n));
+            Console.ReadLine();
+        }
+    }
+}
