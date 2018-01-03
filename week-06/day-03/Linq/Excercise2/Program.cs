335868233a0c4a4d5e818a411706edfc581bfa32
Marton Revesz <martonrevesz@gmail.com>
2018-01-03 13:36:22 +0100

    fix(% operator): rewrite condition

---
+++ week-06/day-03/Linq/Excercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            double average = numbers.Where(n => n % 2 == 1 || n % 2 == -1)
+                .Average();
+            Console.WriteLine(average);
+            Console.ReadLine();
+        }
+    }
+}
