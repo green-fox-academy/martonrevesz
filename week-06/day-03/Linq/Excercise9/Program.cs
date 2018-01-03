65e5121f3e40b4f4e97694115072a760262f3773
Marton Revesz <martonrevesz@gmail.com>
2018-01-03 15:04:14 +0100

    feat(Excercise8): create

--- week-06/day-03/Linq/Excercise9/Program.cs
+++ week-06/day-03/Linq/Excercise9/Program.cs
@@ -4,18 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Excercise8
+namespace Excercise9
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string text = "Green Fox Academy corsacAsbest";
-
-            text.Where(s => s.ToString()
-                .ToUpper() == s.ToString())
-                .ToList()
-                .ForEach(s => Console.Write(s));
+            char[] charArray = { 'a', 'v', '4' };
+            string text = string.Join(string.Empty, charArray);
+            Console.WriteLine(text);
             Console.ReadLine();
         }
     }
