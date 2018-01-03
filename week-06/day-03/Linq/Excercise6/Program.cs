2388025a470b2698475fa6bbfeebf6c85ec5b1af
Marton Revesz <martonrevesz@gmail.com>
2018-01-03 14:22:17 +0100

    feat(Excercise6): create

---
+++ week-06/day-03/Linq/Excercise6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "good morning green fox academy";
+
+            Dictionary<char, int> charFrequency = text.GroupBy(n => n)
+                .ToDictionary(n => n.Key, n => n.Count());
+
+            foreach (var item in charFrequency)
+            {
+                Console.WriteLine($"{item.Key} {item.Value}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
