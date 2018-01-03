7f0b14819da93fa9f232e6f72468609ec0519cd8
Marton Revesz <martonrevesz@gmail.com>
2018-01-03 15:16:43 +0100

    feat(Excercise10): create

---
+++ week-06/day-03/Linq/Excercise10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fox> foxList = new List<Fox>()
+            {
+                new Fox("foxy", "asbest", "green"),
+                new Fox("vuk", "badcat", "green"),
+                new Fox("ravasz", "please", "purple"),
+                new Fox("fox", "asbest", "green"),
+                new Fox("foxter", "asbest", "red")
+            };
+
+            foxList.Where(f => f.Color == "green")
+                .ToList()
+                .ForEach(f => Console.WriteLine(f.Name));
+
+            Console.WriteLine("***");
+
+            foxList.Where(f => f.Color == "green" && f.Type == "asbest")
+                .ToList()
+                .ForEach(f => Console.WriteLine(f.Name));
+
+            Console.ReadLine();
+        }
+    }
+}
