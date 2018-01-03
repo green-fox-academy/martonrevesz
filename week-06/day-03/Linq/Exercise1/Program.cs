b0d72cac087850d33044469348497907a4b5550f
Marton Revesz <martonrevesz@gmail.com>
2018-01-03 13:04:28 +0100

    feat(excercise1): create

---
+++ week-06/day-03/Linq/Exercise1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            n.Where(i => (i % 2 == 0)).ToList().ForEach(i => Console.WriteLine(i));
+            Console.ReadLine();
+        }
+    }
+}
