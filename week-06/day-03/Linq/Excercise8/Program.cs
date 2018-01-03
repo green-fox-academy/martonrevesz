de3712d7011cb123acc9d0d3df0ed2e1567f4124
Marton Revesz <martonrevesz@gmail.com>
2018-01-03 14:49:12 +0100

    feat(Excercise8): create

---
+++ week-06/day-03/Linq/Excercise8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the uppercase characters in a string!
+
+            string text = "Green Fox Academy corsacAsbest";
+
+            text.Where(s => s.ToString()
+                .ToUpper() == s.ToString())
+                .ToList()
+                .ForEach(s => Console.Write(s));
+            Console.ReadLine();
+        }
+    }
+}
