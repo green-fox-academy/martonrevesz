c06385e50ffaee57074b0b4027fae245f9ca53b9
Marton Revesz <martonrevesz@gmail.com>
2018-01-03 14:31:25 +0100

    feat(Excercise7): create

---
+++ week-06/day-03/Linq/Excercise7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI",
+                "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS", "ACADEMI" };
+
+            cities.Where(c => c.StartsWith("A") && c.EndsWith("I"))
+                .ToList()
+                .ForEach(c => Console.WriteLine(c));
+            Console.ReadLine();
+        }
+    }
+}
