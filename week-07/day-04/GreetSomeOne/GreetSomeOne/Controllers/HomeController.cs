b4277dedde6ca6f080c6a001cafd669d1464c0aa
Marton Revesz <martonrevesz@gmail.com>
2018-01-11 13:55:15 +0100

    feat(GreetSomeOne): create

---
+++ week-07/day-04/GreetSomeOne/GreetSomeOne/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using GreetSomeOne.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreetSomeOne.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        public Person Person { get; set; }
+
+        public HomeController(Person person)
+        {
+            Person = person;
+        }      
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("")]
+        public IActionResult Post(Person person)
+        {
+            Person.Name = person.Name;
+            return RedirectToAction("greet");
+        }
+
+        [HttpGet("greet")]
+        public IActionResult Greet()
+        {
+            return View(Person);
+        }
+    }
+}
---
+++ week-07/day-04/GreetSomeOne/GreetSomeOne/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using GreetSomeOne.Models;
+
+namespace GreetSomeOne
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<Person>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
