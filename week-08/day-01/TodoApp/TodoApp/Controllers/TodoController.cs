9bb193ef67d7c21672088693a7ea7f75dc7b9c8d
Marton Revesz <martonrevesz@gmail.com>
2018-01-15 11:34:44 +0100

    feat(todo): create proj outline

---
+++ week-08/day-01/TodoApp/TodoApp/Controllers/TodoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp.Controllers
+{
+    [Route("todo")]
+    public class TodoController : Controller
+    {
+        [Route("")]
+        [Route("list")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
