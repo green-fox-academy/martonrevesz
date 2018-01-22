3b41262944f2a64f175e38462dc2c953c3bc5bf9
Marton Revesz <martonrevesz@gmail.com>
2018-01-22 11:08:55 +0100

    feat(first): create frontend to api tasks

---
+++ week-09/MondayFirst/MondayFirst/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MondayFirst.Controllers
+{
+    [Route("/api/[controller]")]
+    public class ValuesController : Controller
+    {
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+
+    }
+}
