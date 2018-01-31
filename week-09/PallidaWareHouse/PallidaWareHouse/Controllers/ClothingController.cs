a7397f99c393def080c4f984a15c75f6edc4f598
Marton Revesz <martonrevesz@gmail.com>
2018-01-31 14:27:56 +0100

    feat(clothes): create endpoints and home view

--- week-09/PallidaWareHouse/PallidaWareHouse/Controllers/ClothingController.cs
+++ week-09/PallidaWareHouse/PallidaWareHouse/Controllers/ClothingController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace PallidaWareHouse.Controllers
 {
+    [Route("warehouse")]
     public class ClothingController : Controller
     {
         public ClothingController(ClothingRepository clothingRepository)
@@ -15,5 +16,23 @@ namespace PallidaWareHouse.Controllers
         }
 
         public ClothingRepository ClothingRepository { get; set; }
+
+        [HttpGet("")]
+        public IActionResult Warehouse()
+        {
+            return View();
+        }
+
+        [HttpPost("summary")]
+        public IActionResult Summary()
+        {
+            return Ok();
+        }
+
+        [HttpGet("query")]
+        public IActionResult Query([FromQuery] int price, [FromQuery] string type)
+        {
+            return Json(null);
+        }
     }
 }
