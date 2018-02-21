122a2d5945b0c2826f7dc03a3106b06e516ff46b
Marton Revesz <martonrevesz@gmail.com>
2018-02-21 08:38:10 +0100

    feat(token create): init project and create token without RSA

---
+++ week-10/CreateTokenTest/CreateTokenTest/Controllers/TokenRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CreateTokenTest.Controllers
+{
+    public class TokenRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ week-10/CreateTokenTest/CreateTokenTest/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace CreateTokenTest.Controllers
+{
+    [Route("api")]
+    public class ValuesController : Controller
+    {
+        [HttpPost]
+        public IActionResult RequestToken([FromBody] TokenRequest request)
+        {
+            if (request.Username == "Jon" && request.Password == "Again, not for production use, DEMO ONLY!")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, request.Username)
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("verygoodkeyandsomemmore"));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "yourdomain.com",
+                    audience: "yourdomain.com",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return BadRequest("Could not verify username and password");
+        }
+    }
+}
