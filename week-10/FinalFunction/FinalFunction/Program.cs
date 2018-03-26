e52dcd0139f729403e42aa75308ab11fc67a920b
Marton Revesz <martonrevesz@gmail.com>
2018-03-26 09:40:21 +0200

    feat(finalfunction): create SelectTheBiggestOddIndexedNumber function

---
+++ week-10/FinalFunction/FinalFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FinalFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArray = new int[0];
+            Console.WriteLine(SelectTheBiggestOddIndexedNumber(testArray));
+        }
+
+        static int SelectTheBiggestOddIndexedNumber(int[] intArray)
+        {
+            int biggest = intArray.Length > 1 ? intArray[1] : throw new Exception();
+            for (int i = 3; i < intArray.Length; i+=2)
+            {
+                if (intArray[i] > biggest)
+                    biggest = intArray[i];
+            }
+            return biggest;
+        }
+    }
+}
